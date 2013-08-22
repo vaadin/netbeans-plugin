6a1cb1c7140e2880d49a43141c46d052e465450b
denisanisimov <denis@vaadin.com>
2013-08-22 17:08:36 +0300

    Connector check against client package.

--- src/org/vaadin/netbeans/editor/analyzer/ConnectorAnalyzer.java
+++ src/org/vaadin/netbeans/editor/analyzer/ConnectorAnalyzer.java
@@ -3,10 +3,13 @@
  */
 package org.vaadin.netbeans.editor.analyzer;
 
+import java.io.IOException;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.AnnotationValue;
@@ -14,13 +17,21 @@ import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.TypeMirror;
 
 import org.netbeans.api.java.source.CompilationInfo;
+import org.netbeans.api.project.FileOwnerQuery;
+import org.netbeans.api.project.Project;
 import org.netbeans.spi.editor.hints.ErrorDescription;
 import org.netbeans.spi.editor.hints.ErrorDescriptionFactory;
 import org.netbeans.spi.editor.hints.Fix;
 import org.netbeans.spi.editor.hints.Severity;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileUtil;
 import org.openide.util.NbBundle;
+import org.vaadin.netbeans.VaadinSupport;
 import org.vaadin.netbeans.code.generator.JavaUtils;
+import org.vaadin.netbeans.code.generator.XmlUtils;
 import org.vaadin.netbeans.editor.VaadinTaskFactory;
+import org.vaadin.netbeans.model.ModelOperation;
+import org.vaadin.netbeans.model.VaadinModel;
 
 import com.sun.source.tree.AnnotationTree;
 import com.sun.source.tree.ExpressionTree;
@@ -49,6 +60,80 @@ public class ConnectorAnalyzer implements TypeAnalyzer {
         }
         checkConnectorValue(type, info, annotation, descriptions);
         checkConnectorClass(type, info, annotation, descriptions);
+
+        checkClientPackage(type, info, annotation, descriptions);
+    }
+
+    @NbBundle.Messages({
+            "# {0} - clientPackage",
+            "notClientPackage=Class''s package is incorrect, it must be inside client package {0}" })
+    private void checkClientPackage( TypeElement type, CompilationInfo info,
+            AnnotationMirror annotation,
+            Collection<ErrorDescription> descriptions )
+    {
+        FileObject fileObject = info.getFileObject();
+        Project project = FileOwnerQuery.getOwner(fileObject);
+        if (project == null) {
+            return;
+        }
+        VaadinSupport support = project.getLookup().lookup(VaadinSupport.class);
+        if (support == null || !support.isEnabled()) {
+            return;
+        }
+        final FileObject[] clientPackage = new FileObject[1];
+        final boolean[] hasWidgetset = new boolean[1];
+        final String[] clientPkgFqn = new String[1];
+        try {
+            support.runModelOperation(new ModelOperation() {
+
+                @Override
+                public void run( VaadinModel model ) {
+                    FileObject gwtXml = model.getGwtXml();
+                    hasWidgetset[0] = gwtXml != null;
+                    if (gwtXml == null) {
+                        return;
+                    }
+                    try {
+                        clientPackage[0] = XmlUtils.getClientWidgetPackage(
+                                model.getGwtXml(), model.getSourcePath(), false);
+                        clientPkgFqn[0] = AbstractJavaFix
+                                .getWidgetsetFqn(gwtXml);
+                        clientPkgFqn[0] = clientPkgFqn[0].substring(0,
+                                clientPkgFqn[0].length()
+                                        - gwtXml.getNameExt().length()
+                                        + XmlUtils.GWT_XML.length())
+                                + model.getSourcePath();
+                    }
+                    catch (IOException ignore) {
+                    }
+                }
+            });
+            if (!hasWidgetset[0]) {
+                // TODO : no widgetset file error
+            }
+            else if (clientPackage[0] == null
+                    || !FileUtil.isParentOf(clientPackage[0],
+                            info.getFileObject()))
+            {
+                if (clientPackage[0] == null) {
+                    // TODO : for fix hint. Package has to be created
+                }
+                // TODO : provide a hint to move class into the client package (usint refactoring)
+                List<Integer> positions = AbstractJavaFix.getElementPosition(
+                        info, type);
+                ErrorDescription description = ErrorDescriptionFactory
+                        .createErrorDescription(Severity.ERROR,
+                                Bundle.notClientPackage(clientPkgFqn[0]),
+                                Collections.<Fix> emptyList(),
+                                info.getFileObject(), positions.get(0),
+                                positions.get(1));
+                descriptions.add(description);
+            }
+        }
+        catch (IOException e) {
+            Logger.getLogger(ConnectorAnalyzer.class.getName()).log(Level.INFO,
+                    null, e);
+        }
     }
 
     @NbBundle.Messages("badConnectorClass=@Connect annotation must attached to ServerConnector subclass")
@@ -67,13 +152,13 @@ public class ConnectorAnalyzer implements TypeAnalyzer {
                     type);
 
             /*
-             * TODO: provide fix hints: 
-             * - analyze @Connect annotation value. 
-             *   If it's AbstactComponent sublcass then add hint to derive class from AbstractComponentConnector
-             *   (if it doesn't have superclass )
-             *   If it's AbstractExtension  sublcass then add hint to derive class from AbstractExtensionConnector 
-             *   (if it doesn't have superclass ) 
-             * - if it has already incompatible superclass then add hint to implement ServerConnector
+             * TODO: provide fix hints: - analyze @Connect annotation value. If
+             * it's AbstactComponent sublcass then add hint to derive class from
+             * AbstractComponentConnector (if it doesn't have superclass ) If
+             * it's AbstractExtension sublcass then add hint to derive class
+             * from AbstractExtensionConnector (if it doesn't have superclass )
+             * - if it has already incompatible superclass then add hint to
+             * implement ServerConnector
              */
             ErrorDescription description = ErrorDescriptionFactory
                     .createErrorDescription(Severity.ERROR,
